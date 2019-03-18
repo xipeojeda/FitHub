2d8d13418f61b1c017c2173243102a1b82726c9d
Jesus <jesusojedapvcs@gmail.com>
2019-03-17 19:49:59 -0700

    Firebase Login/Registration Implementation

---
+++ app/src/main/java/com/example/fithub/RegistrationActivity.java
@@ -0,0 +1,85 @@
+package com.example.fithub;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+public class RegistrationActivity extends AppCompatActivity {
+    private EditText emailTV, passwordTV;
+    private Button regButton;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState)
+    {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+        initializeUI();
+        regButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                registerNewUser();
+            }
+        });
+    }
+
+    private void registerNewUser()
+    {
+        progressBar.setVisibility(View.VISIBLE);
+        String email, password;
+        email = emailTV.getText().toString();
+        password = passwordTV.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(getApplicationContext(), "Please Enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(getApplicationContext(), "Please Enter password...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(getApplicationContext(),"Registration Successful!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility((View.GONE));
+
+                            Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility((View.GONE));
+                        }
+                    }
+                });
+    }
+
+    private void initializeUI()
+    {
+        emailTV = findViewById(R.id.email);
+        passwordTV = findViewById(R.id.password);
+        regButton = findViewById(R.id.register);
+        progressBar = findViewById(R.id.progressBar);
+    }
+}
