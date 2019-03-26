b650d9b6c75b33c1627a842ce921f7e13772fb35
Jesus Ojeda <jesusojedapvcs@gmail.com>
2019-03-25 21:49:38 -0700

    Email Verification Implemented

--- app/src/main/java/com/example/fithub/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/fithub/ForgotPasswordActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,13 +29,15 @@ public class ForgotPasswordActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                String email = editEmail.getText().toString();
+                String email = editEmail.getText().toString().trim();
+
                 if(TextUtils.isEmpty(email))
                 {
                     Toast.makeText(getApplicationContext(), "Enter your email", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if(task.isSuccessful())
