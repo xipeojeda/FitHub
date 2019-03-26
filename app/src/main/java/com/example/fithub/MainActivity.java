f26ed1054370b8f69b03a7e92302ac604a1cc18d
Jesus Ojeda <jesusojedapvcs@gmail.com>
2019-03-25 22:03:59 -0700

    Update to MainActivity.java
    Removed old iteration code

--- app/src/main/java/com/example/fithub/MainActivity.java
+++ app/src/main/java/com/example/fithub/MainActivity.java
@@ -9,32 +9,9 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button registerBtn, loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initializeViews();
-
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(intent);
-            }
-        });
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void initializeViews() {
-        registerBtn = findViewById(R.id.register);
-        loginBtn = findViewById(R.id.login);
     }
 }
