8f1d2de982bec9322c953ed0a9716362f1f6be8d
iFxLp <irvinf413@gmail.com>
2019-03-26 09:02:32 -0700

    Google API CLient Added

--- app/src/main/java/com/example/fithub/MainActivity.java
+++ app/src/main/java/com/example/fithub/MainActivity.java
@@ -7,15 +7,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.fitness.Fitness;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "StepCounter";
+    private static final int REQUEST_OAUTH_REQUEST_CODE = 0x1001;
+
     Button registerBtn, loginBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initializeViews();
+        //Create the Google Api Client
+        mClient = new GoogleApiClient.Builder(this)
+        //select the Fitness Api
+                .addApi(Fitness.SENSORS_API)
+                .addApi(Fitness.SESSIONS_API)
+                .addApi(Fitness.HISTORY_API)
+                .addApi(Fitness.RECORDING_API)
+                //Specify Scopes of access
+                .addScope(FitnessScopes.SCOPE_ACTIVITY_READ)
+                .addScope(FitnessScopes.SCOPE_BODY_READ_WRITE)
+                //provide callbacks
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/fithub/PedometerActivity.java
@@ -0,0 +1,4 @@
+package com.example.fithub;
+
+public class PedometerActivity {
+}
