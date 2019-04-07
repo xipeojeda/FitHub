2bbe005ef3739574c1c84662f70cf96bfc40929c
Jesus <jesusojedapvcs@gmail.com>
2019-04-06 23:11:46 -0700

    Updating AccountFragment

--- app/src/main/java/com/example/fithub/AccountFragment.java
+++ app/src/main/java/com/example/fithub/AccountFragment.java
@@ -9,9 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class AccountFragment extends Fragment {
+
+    //Required empty public constructor
+    public AccountFragment()
+    {
+
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        //Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account,container,false);
     }
 }
