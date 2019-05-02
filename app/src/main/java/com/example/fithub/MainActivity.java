352b9603a1243830ff5ea403bc990d9f78d41992
iFxLp <irvinf413@gmail.com>
2019-05-01 22:57:28 -0700

    Update Pedometer

--- app/src/main/java/com/example/fithub/MainActivity.java
+++ app/src/main/java/com/example/fithub/MainActivity.java
@@ -1,20 +1,36 @@
 package com.example.fithub;
+
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
 
     public static final String TAG = "StepCounter";
     private static final int REQUEST_OAUTH_REQUEST_CODE = 0x1001;
     private GoogleApiClient mClient;
+    private TextView textView;
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "Number of Steps: ";
+    private int numSteps;
+    TextView TvSteps;
+    TextView BtnStart;
+    TextView BtnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +47,34 @@ public class MainActivity extends AppCompatActivity {
                         Intent pedo = new Intent(MainActivity.this, PedometerActivity.class);
                         startActivity(pedo);
                         overridePendingTransition(0,0);
+                        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+                        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                        simpleStepDetector = new StepDetector();
+                        simpleStepDetector.registerListener(this);
+
+                        TvSteps = (TextView) findViewById(R.id.tv_steps);
+                        BtnStart = (Button) findViewById(R.id.btn_start);
+                        BtnStop = (Button) findViewById(R.id.btn_stop);
+
+                        BtnStart.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View arg0) {
+
+                                numSteps = 0;
+                                sensorManager.registerListener(MainActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+                            }
+                        });
+
+
+                        BtnStop.setOnClickListener(new View.OnClickListener() {
+
+                            @Override
+                            public void onClick(View arg0) {
+
+                                sensorManager.unregisterListener(MainActivity.this);
+
+                            }
+                        });
                         break;
                     case R.id.navigation_log:
                         Intent log = new Intent(MainActivity.this, WorkoutDetailsLog.class);
@@ -63,6 +107,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
+    }
 
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+        TvSteps.setText(TEXT_NUM_STEPS + numSteps);
+    }
 }
